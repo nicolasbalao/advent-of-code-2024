00a42cf1755bc5c7d7bdc83a8013d86f7e896d86
nicolas balao <nicolasbalao.pro@gmail.com>
2024-12-02 17:36:09 +0100

    feat(day-02): Part 2 done + readme

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -12,7 +12,8 @@ fn main() {
 
     let input = extract_input();
 
-    println!("Output: {}", red_nosed_reports(input));
+    println!("Part-1: Output: {}", red_nosed_reports(input.clone()));
+    println!("Part-2: Output: {}", red_nosed_reports_with_dampener(input));
 }
 
 fn red_nosed_reports(input: Vec<Vec<u32>>) -> u32 {
@@ -25,6 +26,7 @@ fn red_nosed_reports(input: Vec<Vec<u32>>) -> u32 {
 
         let is_decreasing = report[0] > report[1];
         let mut is_safe = true;
+
         for i in 1..report.len() {
             if (report[i - 1] > report[i]) != is_decreasing {
                 is_safe = false;
@@ -47,6 +49,60 @@ fn red_nosed_reports(input: Vec<Vec<u32>>) -> u32 {
     result
 }
 
+fn red_nosed_reports_with_dampener(input: Vec<Vec<u32>>) -> u32 {
+    let mut result = 0;
+
+    for report in input {
+        if report.len() < 2 {
+            continue;
+        }
+
+        if report_is_safe(&report) {
+            result += 1;
+            continue;
+        }
+
+        let mut dampened_safe = false;
+
+        for i in 0..report.len() {
+            let mut temp = report.clone();
+
+            temp.remove(i);
+
+            if report_is_safe(&temp) {
+                dampened_safe = true;
+                break;
+            };
+        }
+
+        if dampened_safe {
+            result += 1;
+        }
+    }
+
+    result
+}
+
+fn report_is_safe(report: &Vec<u32>) -> bool {
+    if report.len() < 2 {
+        return false;
+    }
+
+    let is_decreasing = report[0] > report[1];
+
+    for i in 1..report.len() {
+        if (report[i - 1] > report[i]) != is_decreasing {
+            return false;
+        }
+
+        let diff = report[i - 1].abs_diff(report[i]);
+
+        if diff < 1 || diff > 3 {
+            return false;
+        }
+    }
+    true
+}
 fn extract_input() -> Vec<Vec<u32>> {
     let mut input = vec![];
 
@@ -67,7 +123,7 @@ fn extract_input() -> Vec<Vec<u32>> {
 
 #[cfg(test)]
 mod test {
-    use crate::red_nosed_reports;
+    use super::*;
 
     #[test]
     fn example() {
@@ -83,5 +139,20 @@ mod test {
         let expected = 2;
 
         assert_eq!(red_nosed_reports(input), expected);
+    }
+    #[test]
+    fn example_part_2() {
+        let input: Vec<Vec<u32>> = vec![
+            vec![7, 6, 4, 2, 1],
+            vec![1, 2, 7, 8, 9],
+            vec![9, 7, 6, 2, 1],
+            vec![1, 3, 2, 4, 5],
+            vec![8, 6, 4, 4, 1],
+            vec![1, 3, 6, 7, 9],
+        ];
+
+        let expected = 4;
+
+        assert_eq!(red_nosed_reports_with_dampener(input), expected);
     }
 }
